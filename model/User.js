910d5ebbaba96c7de384fc7cfa6ee97228817b33
Lautaro <lautaro.pretti@gmail.com>
2022-07-14 23:17:58 -0300

    minor fix

--- model/User.js
+++ model/User.js
@@ -18,10 +18,6 @@ const UserSchema = mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	temporalCode: {
-		type: String,
-		required: true,
-	},
 	date: {
 		type: Date,
 		default: Date.now,
