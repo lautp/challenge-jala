4fb107edd4bcb7a3bbdd03e5292b25079f757d8f
AndresGB <a.galvisbolivar@gmail.com>
2022-07-16 02:41:59 -0500

    route contacts

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,47 @@
-import './App.scss';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Main';
-import LandingPage from './views/LandingPage';
-import Signing from './views/Onboarding/signing';
-import SignUp from './views/Onboarding/SignUp';
-import ForgotPassword from './views/Onboarding/ForgotPassword';
-import SignInWithoutPassword from './views/Onboarding/SignInWithoutPassword';
-import NoMatch from './views/NoMatch';
-import { PrivateRoute } from './components';
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Main";
+import LandingPage from "./views/LandingPage";
+import Signing from "./views/Onboarding/signing";
+import SignUp from "./views/Onboarding/SignUp";
+import ForgotPassword from "./views/Onboarding/ForgotPassword";
+import SignInWithoutPassword from "./views/Onboarding/SignInWithoutPassword";
+import NoMatch from "./views/NoMatch";
+import { PrivateRoute } from "./components";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<LandingPage />
-					</Route>
-					<Route exact path="/signing">
-						<Signing />
-					</Route>
-					<Route exact path="/signup">
-						<SignUp />
-					</Route>
-					<Route exact path="/forgotpassword">
-						<ForgotPassword />
-					</Route>
-					<Route path="/magiclogin" exact>
-						<SignInWithoutPassword />
-					</Route>
-					<PrivateRoute>
-						<Route path="/board">
-							<Main />
-						</Route>
-					</PrivateRoute>
-					<Route path="*">
-						<NoMatch />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/signing">
+            <Signing />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/forgotpassword">
+            <ForgotPassword />
+          </Route>
+          <Route path="/magiclogin" exact>
+            <SignInWithoutPassword />
+          </Route>
+          <PrivateRoute>
+            <Route path="/">
+              <Main />
+            </Route>
+          </PrivateRoute>
+
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
